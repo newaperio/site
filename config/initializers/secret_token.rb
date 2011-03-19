121c181e15c34c55624a846b81db8c6c88d2b671
Logan Leger <logan@loganleger.com>
2011-03-19 18:32:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newaperio11::Application.config.secret_token = '790a8f7d9519bbf43621e45da5dce8d64ad5914d655e6bde72011c3ca584ea13c6679f6503bb3c740320c36bca3b10b647bea7b989f5948dabf078e9356ba4d9'
