672a166d84d9cf564768e990a26c677c6cfb3f00
Logan Leger <logan@loganleger.com>
2011-03-23 01:00:48 -0500

    Portfolio coming soon; copy updates to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Newaperio11::Application.routes.draw do
 	scope "/" do
 		get 'about' => 'static#about', :as => :about
-		# match 'portfolio' => 'static#portfolio', :as => :portfolio
+		get 'portfolio' => 'static#portfolio', :as => :portfolio
 		get 'contact' => 'static#contact', :as => :contact
 	end
 	
