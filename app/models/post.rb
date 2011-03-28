f58015a56ae3cb9e64f246f560c41950b2538c05
Logan Leger <logan@loganleger.com>
2011-03-28 02:15:38 -0500

    Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ActiveRecord::Base
 	belongs_to :category
 	has_many :comments
 	
+	validates_presence_of :title
+	validates_presence_of :author_id
+	validates_presence_of :category_id
+	validates_presence_of :content
+	
 	def to_param
     "#{id}-#{title.downcase.gsub(/[^[:alnum:]]/,'-')}".gsub(/-{2,}/,'-')
   end 
