16efcf23e70732ba7ebc7553c2a438b2267d09a7
Logan Leger <logan@loganleger.com>
2011-03-28 19:53:43 -0500

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,23 @@
 Newaperio11::Application.routes.draw do
-	scope "/" do
-		get 'about' => 'static#about', :as => :about
-		get 'portfolio' => 'static#portfolio', :as => :portfolio
-		get 'contact' => 'static#contact', :as => :contact
-		scope "blog" do
-			scope "admin" do
-				get "logout" => "sessions#destroy", :as => :logout
-				get "login" => "sessions#new", :as => :login
-				post "login" => "sessions#create", :as => :do_login
-				get "/" => "blog_admin#index", :as => :blog_admin_index				
-				constraints lambda { |req| req.session[:author_id].present? } do
-					post "/" => "blog_admin#create", :as => :blog_admin_create
-					get "/new" => "blog_admin#new", :as => :blog_admin_new
-					get "/:id/edit" => "blog_admin#edit", :as => :blog_admin_edit
-					put "/:id" => "blog_admin#update", :as => :blog_admin_update
-					delete "/:id" => "blog_admin#destroy", :as => :blog_admin_destroy
-				end
+	get 'about' => 'static#about', :as => :about
+	get 'portfolio' => 'static#portfolio', :as => :portfolio
+	get 'contact' => 'static#contact', :as => :contact
+	scope "blog" do
+		scope "admin" do
+			get "logout" => "sessions#destroy", :as => :logout
+			get "login" => "sessions#new", :as => :login
+			post "login" => "sessions#create", :as => :do_login
+			get "/" => "blog_admin#index", :as => :blog_admin_index				
+			constraints lambda { |req| req.session[:author_id].present? } do
+				post "/" => "blog_admin#create", :as => :blog_admin_create
+				get "/new" => "blog_admin#new", :as => :blog_admin_new
+				get "/:id/edit" => "blog_admin#edit", :as => :blog_admin_edit
+				put "/:id" => "blog_admin#update", :as => :blog_admin_update
+				delete "/:id" => "blog_admin#destroy", :as => :blog_admin_destroy
 			end
-			get "/" => "posts#index", :as => :blog
-			get "/:id" => "posts#show", :as => :post
 		end
+		get "/" => "posts#index", :as => :blog
+		get "/:id" => "posts#show", :as => :post
 	end
-	root :to => "static#home"
+		root :to => "static#home"
 end
