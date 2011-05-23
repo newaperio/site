e3fd59ab647ccc7f3b29501018ef66fb6f533fa4
Evan Cordell <cordell.evan@gmail.com>
2011-05-23 18:21:22 -0500

    Added coderay syntax highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,12 @@ module ApplicationHelper
     Textilizer.new(text).to_html.html_safe unless text.blank?
   end
   
+  def coderay(text)
+    text.gsub(/\<code( lang="(.+?)")?\>(.+?)\<\/code\>/m) do
+      content_tag('notextile', CodeRay.scan($3, $2).div(:css => :class).html_safe)
+    end
+  end
+  
   def title(page_title)
     content_for(:title) { page_title }
   end
