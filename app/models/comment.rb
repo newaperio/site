e8a3bc108d6094730c9bf7038b316c013e07ea28
Logan Leger <logan@loganleger.com>
2011-07-01 10:47:59 -0500

    Check for http on comments, add if not present

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ActiveRecord::Base
 	belongs_to :post
 	
 	validates :name, :email, :content, :presence => true
+	
+	before_save :http
+	
+	private
+	
+	def http
+   unless self.url =~ /http[s]?:\/\//
+     self.url = "http://" + self.url
+   end
+	end
 end
