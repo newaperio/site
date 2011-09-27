c8fe37b3468c530e5afe370a13e634e576211d18
Logan Leger <logan@loganleger.com>
2011-09-27 14:28:25 -0500

    Update cap file

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,9 +21,19 @@ set :config_files, %w( database.yml )
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
+  
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
-		run "chmod -R 777 #{release_path}/public/"
+  end
+  
+  task :make_public do
+    run "chmod -R 777 #{release_path}/public/"
+  end
+  
+  task :database_yml do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"     
   end
 end
+
+after "deploy:update_code", "deploy:database_yml"
+before "deploy:restart", "deploy:make_public"
