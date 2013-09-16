69ba8dbecd78efefede90d6da2c925d5f01e54c5
Logan Leger <logan@loganleger.com>
2013-09-16 15:49:52 -0500

    Use env var for secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Newaperio11::Application.config.secret_token = '7e349f1a4d840e35e267a0c6a5ea0fdbebd7cf4184ff4cb52129a919099a08361c57fab7090284e6a7a346894847390b2ce9253dc1d85a8f92a0ac3dd3eefc8a'
+Newaperio11::Application.config.secret_token = ENV['SECRET_TOKEN']
