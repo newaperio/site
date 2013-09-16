fb78c488de25b13e9357e111c750370fe83c70d1
Logan Leger <logan@loganleger.com>
2013-09-16 15:05:41 -0500

    Updating unicorn config
    - Use Heroku process signals
    - Use env vars

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,17 +1,24 @@
-worker_processes 3
-timeout 30
+worker_processes (ENV['WEB_CONCURRENCY'] || 3).to_i
+timeout (ENV['WEB_TIMEOUT'] || 5).to_i
 preload_app true
- 
+
 before_fork do |server, worker|
-  if defined?(ActiveRecord::Base)
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  if defined? ActiveRecord::Base
     ActiveRecord::Base.connection.disconnect!
-    Rails.logger.info('Disconnected from ActiveRecord')
   end
 end
- 
+
 after_fork do |server, worker|
-  if defined?(ActiveRecord::Base)
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  if defined? ActiveRecord::Base
     ActiveRecord::Base.establish_connection
-    Rails.logger.info('Connected to ActiveRecord')
   end
 end
