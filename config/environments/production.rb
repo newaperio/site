9c852787ab58a96dd4079d3ddee6d9cf25b45a0b
Logan Leger <logan@loganleger.com>
2016-07-28 15:13:38 -0500

    Change asset host to allow SSL
    We have SSL enabled now, but we don't have the SSL cert on CF so the
    CNAME won't work here. Instead just use the CF host, which is fine.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Newaperio11::Application.configure do
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "//assets.newaperio.com"
+  config.action_controller.asset_host = "//d2q2tm8k4cxoln.cloudfront.net"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -46,7 +46,7 @@ Newaperio11::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
@@ -55,6 +55,6 @@ Newaperio11::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  
+
   config.assets.precompile += %w(modernizr.js)
 end
